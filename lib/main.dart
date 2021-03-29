9cf305155db7029c637471e5ebd6c73cbca87a92
ummesalma-saifuddin <54745376+ummesalma-saifuddin@users.noreply.github.com>
2021-03-29 23:59:07 +0500

    homepage-camera and gallery selector done

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,11 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'dart:async';
 
-void main(){
+void main() {
   runApp(new MaterialApp(
     home: new MyApp(),
   ));
 }
-
 
 class MyApp extends StatefulWidget {
   @override
@@ -22,14 +21,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return new SplashScreen(
-      seconds: 5,
-      navigateAfterSeconds: new AfterSplash(),
-      image: new Image.asset('assets/Logo5.png'), // #1b0753
-      backgroundColor: Colors.black87,
-      styleTextUnderTheLoader: new TextStyle(),
-      photoSize: 120.0,
-      loaderColor: Colors.blue.shade300
-    );
+        seconds: 5,
+        navigateAfterSeconds: new AfterSplash(),
+        image: new Image.asset('assets/Logo5.png'), // #1b0753
+        backgroundColor: Colors.black87,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 120.0,
+        loaderColor: Colors.blue.shade300);
   }
 }
 
@@ -43,14 +41,13 @@ class AfterSplash extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() =>  _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 GlobalKey<_MyHomePageState> globalKey = GlobalKey();
@@ -59,8 +56,19 @@ class _MyHomePageState extends State<MyHomePage> {
   File _image;
   final picker = ImagePicker();
 
-  Future <Null> newBttn() async {
+  Future<Null> imageSelector() async {
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future<Null> cameraSelector() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
     setState(() {
       if (pickedFile != null) {
         _image = File(pickedFile.path);
@@ -75,20 +83,25 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-  Container imzButton(String itext, Color icolor, IconData iicon, void Function() callback) {
+  Container imzButton(
+      String itext, Color icolor, IconData iicon, void Function() callback) {
     return Container(
-      width: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.2,
-        decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black87,
-            spreadRadius: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.005,
-            blurRadius: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.005,
-          ),
-        ],
+      width: 20.0,
+      decoration: BoxDecoration(
+        // boxShadow: [
+        //   BoxShadow(
+        //     color: Colors.black87,
+        //     spreadRadius: (MediaQuery.of(context).size.height -
+        //             MediaQuery.of(context).padding.top) *
+        //         0.005,
+        //     blurRadius: (MediaQuery.of(context).size.height -
+        //             MediaQuery.of(context).padding.top) *
+        //         0.005,
+        //   ),
+        // ],
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(25.25),
-          bottomRight: Radius.circular(25.25)),
+            topLeft: Radius.circular(25.25),
+            bottomRight: Radius.circular(25.25)),
         border: Border(
           top: BorderSide(width: 1.0, color: icolor),
           left: BorderSide(width: 1.0, color: icolor),
@@ -96,11 +109,11 @@ class _MyHomePageState extends State<MyHomePage> {
           bottom: BorderSide(width: 1.0, color: icolor),
         ),
       ),
-      child:  Column(
+      child: Column(
         children: [
           Container(
-            width: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.15,
-            height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.1,
+            width: 20,
+            height: 20,
             child: IconButton(
               iconSize: 50,
               icon: Icon(iicon),
@@ -118,76 +131,208 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildImage(BuildContext context) {
-    return  Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size.fromHeight(0),
-          child: AppBar( // Here we create one to set status bar color
-            backgroundColor: Colors.black, // Set any color of status bar you want; or it defaults to your theme's primary color
-          )
-        ),
-      body: ListView(
-        children: <Widget>[
-          
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.1,
-          decoration: BoxDecoration(
-            color: Colors.black54,
-            image: const DecorationImage(
-              image: AssetImage('assets/Logo5.png'),
-              scale: 2,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("IMAZING"),
+        // Here we create one to set status bar color
+        backgroundColor: Colors
+            .black, // Set any color of status bar you want; or it defaults to your theme's primary color
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.save,
+              color: Colors.white,
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black87.withOpacity(0.7),
-                spreadRadius: 10,
-                blurRadius: 10,
-              ),
-            ],
-          ),
-        ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+      ),
+      body: Align(
+        child: Row(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () async {
+                          await imageSelector();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 2 - 20,
+                          height: MediaQuery.of(context).size.width / 2,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: <Widget>[
+                              Container(
+                                width:
+                                    MediaQuery.of(context).size.width / 2 - 14,
+                                height: MediaQuery.of(context).size.width /
+                                    2 /
+                                    0.6625,
+                                decoration: BoxDecoration(
+                                  color: Colors.white70,
+                                  border:
+                                      Border.all(color: Colors.black, width: 3),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(16),
+                                  child: Opacity(
+                                      opacity: 1,
+                                      child: Image.asset(
+                                        'assets/images/Logo1.png',
+                                        fit: BoxFit.cover,
+                                      )),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 60.0),
+                                child: Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.black, width: 1),
+                                        color: Colors.black.withOpacity(0.2),
+                                        shape: BoxShape.circle),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: Icon(
+                                        Icons.image,
+                                        color: Colors.black,
+                                        size: 40,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Gallery",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () async {
+                          await cameraSelector();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 2 - 20,
+                          height: MediaQuery.of(context).size.width / 2,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: <Widget>[
+                              Container(
+                                width:
+                                    MediaQuery.of(context).size.width / 2 - 14,
+                                height: MediaQuery.of(context).size.width /
+                                    2 /
+                                    0.6625,
+                                decoration: BoxDecoration(
+                                  color: Colors.white70,
+                                  border:
+                                      Border.all(color: Colors.black, width: 3),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(16),
+                                  child: Opacity(
+                                      opacity: 1,
+                                      child: Image.asset(
+                                        'assets/images/Logo1.png',
+                                        fit: BoxFit.cover,
+                                      )),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 60.0),
+                                child: Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.black, width: 1),
+                                        color: Colors.black.withOpacity(0.2),
+                                        shape: BoxShape.circle),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: Icon(
+                                        Icons.camera_alt_outlined,
+                                        color: Colors.black,
+                                        size: 40,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Camera",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ],
+            ),
 
-        
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.75,
-          color: Colors.black87,
+            // Padding(
+            //   padding: const EdgeInsets.fromLTRB(0, 0, 0, 32),
+            //   child: Container(
+            //     width: MediaQuery.of(context).size.width * 0.8,
+            //     child: Text(
+            //       "Select image, and edit them easily in a matter of seconds. Save or share them wherever and whenever you want.",
+            //       textAlign: TextAlign.center,
+            //       style: TextStyle(
+            //         fontSize: 13,
+            //         color: Theme.of(context).hintColor,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+          ],
         ),
-
-        Container(
-          height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.15,
-          decoration: BoxDecoration(
-            color: Colors.black54.withOpacity(0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black87,
-                spreadRadius: 10,
-                blurRadius: 10,
-              ),
-            ],
-          ),
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              imzButton('New Image', Colors.blueGrey.shade700, Icons.photo, newBttn),
-              Container(width: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.02),
-              imzButton('Quick Save', Colors.blueAccent.shade700, Icons.save, newBttn),
-              Container(width: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.02),
-              imzButton('Apply Filters', Colors.lightBlueAccent.shade700, Icons.science, newBttn),
-              Container(width: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.02),
-              imzButton('Pro-Editor', Colors.brown.shade700, Icons.handyman, newBttn),
-              Container(width: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.02),
-              imzButton('Compress <1MB', Colors.teal.shade700, Icons.save_alt, newBttn),
-            ],
-          ),
-        ),
-
-      ]),
+      ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
-
-    return  _buildImage(context);
+    return _buildImage(context);
   }
 }
